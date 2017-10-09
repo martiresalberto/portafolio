4e61d2f53347d9c8270c9f6b8494829be588c7c6
Martires <trans_maog@htomail.com>
2017-10-09 09:12:46 -0600

    hacerlo mejor

--- database/migrations/2017_07_26_205650_create_products_table.php
+++ database/migrations/2017_07_26_205650_create_products_table.php
@@ -22,7 +22,7 @@ class CreateProductsTable extends Migration
             $table->string('image', 300);
             $table->boolean('visible');
             $table->integer('category_id')->unsigned();
-            $table->foreign('category_id')
+            $table->dropforeign('category_id')
                   ->references('id')
                   ->on('categories')
                   ->onDelete('cascade');
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,11 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         //$this->call(UsersTableSeeder::class);
-         //$this->call(CategoryTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
+         $this->call(CategoryTableSeeder::class);
          $this->call(ProductTableSeeder::class);
-         //$this->call(ServiceTableSeeder::class);
-         //$this->call(AboutTableSeeder::class);
-         //$this->call(CertificationTableSeeder::class);
+         $this->call(ServiceTableSeeder::class);
+         $this->call(AboutTableSeeder::class);
+         $this->call(CertificationTableSeeder::class);
     }
 }
