cc8ec366b042cd9c4938a856d0d91df886f8d5e3
Martires <trans_maog@htomail.com>
2017-10-12 10:30:31 -0600

    subiendo servicios de portafolio

--- database/seeds/ServiceTableSeeder.php
+++ database/seeds/ServiceTableSeeder.php
@@ -14,15 +14,32 @@ class ServiceTableSeeder extends Seeder
     {
         $data = array(
 			[
-				'name' 			=> 'JAVASCRIPT',
-				'slug' 			=> 'JAVASCRIPT',
-				'description' 	=> 'es un lenguaje de programación interpretado, diale...',
-				'image' 		=> 'http://gdurl.com/s_d6',
+				'name' 			=> 'BOOTSTRAP',
+				'slug' 			=> 'BOOTSTRAP',
+				'description' 	=> 'es un framework o conjunto de herramientas de Código abierto para diseño de sitios y aplicaciones web. ,
+				'image' 		=> 'http://gdurl.com/h1Qh',
 				'created_at' 	=> new DateTime,
 				'updated_at' 	=> new DateTime,
 				'category_id' 	=> 1
 			],
-			
+			[
+				'name' 			=> 'PHP',
+				'slug' 			=> 'PHP',
+				'description' 	=> ' es un lenguaje de programación interpretado, diseñado originalmente para la creación de páginas web dinámicas ,
+				'image' 		=> 'http://gdurl.com/6HtE',
+				'created_at' 	=> new DateTime,
+				'updated_at' 	=> new DateTime,
+				'category_id' 	=> 1
+			],
+			[
+				'name' 			=> 'LARAVEL',
+				'slug' 			=> 'LARAVEL',
+				'description' 	=> 'es un framework de código abierto para desarrollar aplicaciones y servicios web con PHP 5 y PHP 7. ,
+				'image' 		=> 'http://gdurl.com/MmJH',
+				'created_at' 	=> new DateTime,
+				'updated_at' 	=> new DateTime,
+				'category_id' 	=> 1
+			],
 	
 			
 		);
