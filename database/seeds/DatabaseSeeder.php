70c5def276f3314d77d0de0ebdea5d9873a04458
Martires <trans_maog@htomail.com>
2017-10-11 23:55:20 -0600

    actualizacion de codigo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          //$this->call(UsersTableSeeder::class);
-         //$this->call(CategoryTableSeeder::class);
-         //$this->call(ProductTableSeeder::class);
+         $this->call(CategoryTableSeeder::class);
+         $this->call(ProductTableSeeder::class);
          $this->call(ServiceTableSeeder::class);
-         //$this->call(AboutTableSeeder::class);
-         //$this->call(CertificationTableSeeder::class);
+         $this->call(AboutTableSeeder::class);
+         $this->call(CertificationTableSeeder::class);
     }
 }
