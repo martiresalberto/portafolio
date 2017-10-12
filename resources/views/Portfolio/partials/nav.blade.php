5bcbf0bf1ef1dff6e1f47414231997de3a4ffc1e
Martires <trans_maog@htomail.com>
2017-10-12 00:12:29 -0600

    actualizacion de nombre del desarrollador

--- resources/views/Portfolio/partials/nav.blade.php
+++ resources/views/Portfolio/partials/nav.blade.php
@@ -9,7 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand page-scroll" href="#page-top">Desarrollador Web</a>
+                <a class="navbar-brand page-scroll" href="#page-top">Martires-Alberto-Orozco</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
