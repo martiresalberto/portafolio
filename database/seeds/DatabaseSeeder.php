d3d852c3f1a8189fb8fe73258dcd9fc15612bc17
Martires <trans_maog@htomail.com>
2017-10-12 13:35:39 -0600

    subiendo servicios de portafolio

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
+         //$this->call(UsersTableSeeder::class);
          $this->call(CategoryTableSeeder::class);
          $this->call(ProductTableSeeder::class);
-         //$this->call(ServiceTableSeeder::class);
+         $this->call(ServiceTableSeeder::class);
          //$this->call(AboutTableSeeder::class);
          //$this->call(CertificationTableSeeder::class);
     }
