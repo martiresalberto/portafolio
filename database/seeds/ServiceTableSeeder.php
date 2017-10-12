27e228f97c6814f028aface0e622cb299041e9ae
Martires <trans_maog@htomail.com>
2017-10-12 10:32:43 -0600

    subiendo servicios de portafolio

--- database/seeds/ServiceTableSeeder.php
+++ database/seeds/ServiceTableSeeder.php
@@ -10,40 +10,40 @@ class ServiceTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        $data = array(
-			[
-				'name' 			=> 'BOOTSTRAP',
-				'slug' 			=> 'BOOTSTRAP',
-				'description' 	=> 'es un framework o conjunto de herramientas de Código abierto para diseño de sitios y aplicaciones web. ,
-				'image' 		=> 'http://gdurl.com/h1Qh',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
-			],
-			[
-				'name' 			=> 'PHP',
-				'slug' 			=> 'PHP',
-				'description' 	=> ' es un lenguaje de programación interpretado, diseñado originalmente para la creación de páginas web dinámicas ,
-				'image' 		=> 'http://gdurl.com/6HtE',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
-			],
-			[
-				'name' 			=> 'LARAVEL',
-				'slug' 			=> 'LARAVEL',
-				'description' 	=> 'es un framework de código abierto para desarrollar aplicaciones y servicios web con PHP 5 y PHP 7. ,
-				'image' 		=> 'http://gdurl.com/MmJH',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
-			],
-	
-			
-		);
+ public function run()
+   {
+    $data = array(
+		[
+			'name' 			=> 'BOOTSTRAP',
+			'slug' 			=> 'BOOTSTRAP',
+			'description' 	=> 'es un framework o conjunto de herramientas de Código abierto para diseño de sitios y aplicaciones web.' ,
+			'image' 		=> 'http://gdurl.com/h1Qh',
+			'created_at' 	=> new DateTime,
+			'updated_at' 	=> new DateTime,
+			'category_id' 	=> 1
+		],
+		[
+			'name' 			=> 'PHP',
+			'slug' 			=> 'PHP',
+			'description' 	=> 'es un lenguaje de programación interpretado, diseñado originalmente para la creación de páginas web dinámicas',
+			'image' 		=> 'http://gdurl.com/6HtE',
+			'created_at' 	=> new DateTime,
+			'updated_at' 	=> new DateTime,
+			'category_id' 	=> 1
+		],
+		[
+			'name' 			=> 'LARAVEL',
+			'slug' 			=> 'LARAVEL',
+			'description' 	=> 'es un framework de código abierto para desarrollar aplicaciones y servicios web con PHP 5 y PHP 7.',
+			'image' 		=> 'http://gdurl.com/MmJH',
+			'created_at' 	=> new DateTime,
+			'updated_at' 	=> new DateTime,
+			'category_id' 	=> 1
+		],
 
-		Service::insert($data);
+		
+	);
+
+	Service::insert($data);
     }
 }
