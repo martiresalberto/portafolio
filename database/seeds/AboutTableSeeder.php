15cf9339ccb10ad8e2716451b8113030c06ecef6
Martires <trans_maog@htomail.com>
2017-10-12 22:15:53 -0600

    Commit de About

--- database/seeds/AboutTableSeeder.php
+++ database/seeds/AboutTableSeeder.php
@@ -16,7 +16,7 @@ class AboutTableSeeder extends Seeder
 			[
 				'name' => 'DESARROLLADOR WEB | FRONT-END | BACK=END', 
 				'slug' => 'DESARROLLADOR WEB | FRONT-END | BACK=END-1', 
-				'description' => 'Soy un programador web junior con conocimientos básicos para la creación de paginas web soy muy apasionado y me entrego al máximo a continuación detalle mis habilidades . *FRONT-END: Html5 , Css3 , Bootstrap , y Plantillas. *BACKEND:Php , Javascript y el Framework LARAVEL en su versión 5.2, 5.3 , 5.4.*Datos:Sql , Mysql', 
+				'description' => 'Soy un programador Y desarrollador web con conocimientos básicos para la creación de paginas web soy muy apasionado y me entrego al máximo en todos los proyectos a continuación detalle mis habilidades . *FRONT-END: Html5 , Css3 , Bootstrap , y Plantillas. *BACKEND:Php , Javascript y el Framework LARAVEL en su versión 5.2, 5.3 , 5.4.*Datos:Sql , Mysql', 
 				'image' => 'http://gdurl.com/oJNX'
 			],
 			
