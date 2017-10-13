a837c6f4dd7e2c4fccdffeaa0568d46a70ffe669
Martires <trans_maog@htomail.com>
2017-10-12 22:29:28 -0600

    Commit de Certificaciones

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
          //$this->call(CategoryTableSeeder::class);
          //$this->call(ProductTableSeeder::class);
          //$this->call(ServiceTableSeeder::class);
-         $this->call(AboutTableSeeder::class);
-         //$this->call(CertificationTableSeeder::class);
+         //$this->call(AboutTableSeeder::class);
+         $this->call(CertificationTableSeeder::class);
     }
 }
