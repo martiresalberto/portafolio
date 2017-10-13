03dfd6f1bcc7359b07cdc829117e175b21824e5b
Martires <trans_maog@htomail.com>
2017-10-12 22:56:26 -0600

    Commit de Certificaciones

--- database/seeds/ServiceTableSeeder.php
+++ database/seeds/ServiceTableSeeder.php
@@ -19,18 +19,14 @@ class ServiceTableSeeder extends Seeder
 				'slug' 			=> 'HTML5',
 				'description' 	=> 'HTML5 es la última versión de HTML.El término representa dos conceptos diferentes:Se trata de una nueva versión con nuevos elementos, atributos ',
 				'image' 		=> 'http://gdurl.com/LnQQ',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
+				
 			],
 			[
 				'name' 			=> 'CSS3',
 				'slug' 			=> 'CSS3',
 				'description' 	=> 'es un lenguaje usado para definir la presentación de un documento estructurado escrito en HTML.',
 				'image' 		=> 'http://gdurl.com/X-0u',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
+				
 			],
 			[
 				'name' 			=> 'JAVASCRIP',
@@ -38,36 +34,28 @@ class ServiceTableSeeder extends Seeder
 				'description' 	=> 'es un lenguaje de programación interpretado, dialecto del estándar ECMAScript. Se define como orientado a objetos,​ basado en prototipos, imperativo, débilmente tipado y dinámico.
 ',
 				'image' 		=> 'http://gdurl.com/s_d6',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
+				
 			],
 			[
 				'name' 			=> 'BOOTSTRAP',
 				'slug' 			=> 'BOOTSTRAP',
 				'description' 	=> 'es un framework o conjunto de herramientas de Código abierto para diseño de sitios y aplicaciones web.',
 				'image'         => 'http://gdurl.com/h1Qh',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
+				
 			],
 			[
 				'name' 			=> 'PHP',
 				'slug' 			=> 'PHP',
 				'description' 	=> 'Es un lenguaje de programación interpretado, diseñado originalmente para la creación de páginas web dinámicas.',
 				'image' 		=> 'http://gdurl.com/6HtE',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
+				
 			],
 			[
 				'name' 			=> 'LARAVEL 5.3 y 5.4',
 				'slug' 			=> 'LARAVEL 5.3 y 5.4',
 				'description' 	=> 'Es un framework de código abierto para desarrollar aplicaciones y servicios web con PHP 5 y PHP 7.',
 				'image'         => 'http://gdurl.com/MmJH',
-				'created_at' 	=> new DateTime,
-				'updated_at' 	=> new DateTime,
-				'category_id' 	=> 1
+				
 			],
 		
 	);
